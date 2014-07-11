e86342bbe3a71dcae9782f73c53f1efd3c273b2b
Federico Silva <FedericoSilva@Federicos-MacBook-Pro.local>
2014-07-11 16:01:32 -0400

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Grid.m
+++ GameOfLife.spritebuilder/Grid.m
@@ -71,4 +71,12 @@ static const int GRID_COLUMNS = 10;
     
 }
 
+- (Creature *)creatureForTouchPosition:(CGPoint)touchPosition {
+    int row = touchPosition.y / _cellHeight;
+    int column = touchPosition.x / _cellWidth;
+    
+    return _gridArray[row][column];
+    
+
+}
 @end
