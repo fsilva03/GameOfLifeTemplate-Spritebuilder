76050e02a3ab32dd973279c44a35b0e2f6440c6a
Federico Silva <FedericoSilva@Federicos-MacBook-Pro.local>
2014-07-14 16:40:47 -0400

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Source/MainScene.m
+++ GameOfLife.spritebuilder/Source/MainScene.m
@@ -40,6 +40,7 @@
 }
 
 -(void)step {
+    
     [_grid evolveStep];
     _generationLabel.string = [NSString stringWithFormat:@"%d", _grid.generation];
     _populationLabel.string = [NSString stringWithFormat:@"%d", _grid.totalAlive];
